cc54971efff71f1d738d57184b04826f3f2f43ac
devDaviSousa <devsousadavi@gmail.com>
2022-01-27 17:05:26 -0300

    Alterando informações na view

--- CursoMVC/Models/Categoria.cs
+++ CursoMVC/Models/Categoria.cs
@@ -1,9 +1,16 @@
-﻿namespace CursoMVC.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CursoMVC.Models
 {
     public class Categoria
     {
         public int Id { get; set; }
 
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage ="O campo decrição é obrigatorio")]
         public string Descricao { get; set; }   
+
+        public List<Produto> Produtos { get; set; }
     }
 }
